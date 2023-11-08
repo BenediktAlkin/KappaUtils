a50070a784ce60819f211e2403c483aa58a4052f
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-11-08 19:24:08 +0100

    Update writers.py

--- kappautils/images/png/writers.py
+++ kappautils/images/png/writers.py
@@ -10,10 +10,11 @@ def png_writer_greyscale(img, fp):
     to_pil_image(img, mode="L").save(fp)
 
 
-def png_writer_viridis(img, fp):
+# noinspection PyShadowingBuiltins
+def png_writer_viridis(img, fp, format=None):
     if img.ndim == 2:
         img = img.unsqueeze(0)
     assert img.ndim == 3 and img.size(0) == 1
     img = to_pil_image(img, mode="L")
     img.putpalette(VIRIDIS_PALETTE_LIST)
-    img.save(fp)
+    img.save(fp, format=format)
