96cc76f9399cfb9ecfb19caccf6be24436f08dd7
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-12-02 23:30:33 +0100

    fix

--- kappautils/images/points_to_image.py
+++ kappautils/images/points_to_image.py
@@ -27,13 +27,15 @@ def coords_to_image(coords, resolution, coords_min=None, coords_max=None, weight
         assert resolution is not None
         resolution = torch.tensor(to_2tuple(resolution), device=coords.device)
     assert resolution.ndim == 1 and resolution.numel() == 2
-    # rescale coords to resolution
+    # rescale coords to [0, inf]
     if coords_min is not None:
         if not torch.is_tensor(coords_min):
             coords_min = torch.tensor(coords_min, device=coords.device)
     else:
         coords_min = coords.min(dim=0).values
     assert coords_min.ndim == 1 and coords_min.numel() == 2
+    coords = coords - coords_min.unsqueeze(0)
+    # rescale coords to [0, resolution]
     if coords_max is not None:
         if not torch.is_tensor(coords_max):
             coords_max = torch.tensor(coords_max, device=coords.device)
@@ -41,7 +43,8 @@ def coords_to_image(coords, resolution, coords_min=None, coords_max=None, weight
         coords_max = coords.max(dim=0).values
     assert coords_max.ndim == 1 and coords_max.numel() == 2
     coords_max = coords_max / (resolution - 1)
-    coords = (coords - coords_min.unsqueeze(0)) / coords_max.unsqueeze(0)
+    coords = coords / coords_max.unsqueeze(0)
+    # snap to closest pixel
     coords = coords.round().long()
     # flatten coords (e.g. [4, 6] with resolution=(6, 8) will be flattened to 4 * 8 + 6)
     coords = coords[:, 0] * resolution[1] + coords[:, 1]
--- tests_unit/images/test_points_to_image.py
+++ tests_unit/images/test_points_to_image.py
@@ -8,7 +8,7 @@ from kappautils.images.points_to_image import xy_to_image
 class TestPointsToImage(unittest.TestCase):
     def test_shape(self):
         rng = torch.Generator().manual_seed(8493)
-        x = torch.rand(100, generator=rng) * 4
+        x = torch.rand(100, generator=rng) * 4 - 1
         y = torch.rand(100, generator=rng) * 2
         weights = torch.rand(100, generator=rng)
         img = xy_to_image(
